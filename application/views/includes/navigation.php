b71e2f4dc439d0d79039bad8f11cda7487053b63
batoulalsharef <batoulalsharef386@gmail.com>
2018-04-04 01:18:04 +0300

    files for design

---
+++ application/views/includes/navigation.php
@@ -0,0 +1,8 @@
+<nav>
+						<ul class="primary-nav">
+							<li><a href="#intro">The collective</a></li>
+							<li><a href="#team">The crew</a></li>
+							<li><a href="#articles">Articles</a></li>
+							<li><a href="#freebies">Freebies</a></li>
+						</ul>
+					</nav>
