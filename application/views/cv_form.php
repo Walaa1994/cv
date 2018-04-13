31279695c7c096ec3401955d1b6466a874020730
Walaa1994 <walaa.albaghdadi@hotmail.com>
2018-04-13 10:04:51 +0300

    modified cv_form view

--- application/views/cv_form.php
+++ application/views/cv_form.php
@@ -4,11 +4,11 @@
 <head>
 
   <meta charset="UTF-8">
-  <title>our app</title>  
+  <title>CV Form</title>  
   <link href="<?php echo base_url();?>/assets/css/style.css" rel='stylesheet' type='text/css' />
   <style>
 form#multiphase{ border:#000 1px solid; padding:24px; width:350px; }
-form#multiphase > #phase2, #phase3, #show_all_data{ display:none; }
+form#multiphase > #phase2, #phase3, #phase4, #phase5, #phase6{ display:none; }
 </style>
   <script>
 var fname, lname, gender, country;
@@ -16,22 +16,22 @@ function _(x){
   return document.getElementById(x);
 }
 function processPhase1(){
-  fname = _("first_name").value;
+  /*fname = _("first_name").value;
   lname = _("last_name").value;
   city= _("city").value;
   email= _("email").value;
   gender= _("gender").value;
-  if(fname.length > 2 && lname.length > 2 && city.length > 2 && email.length > 2 && gender.length > 0){
+  if(fname.length > 2 && lname.length > 2 && city.length > 2 && email.length > 2 && gender.length > 0){*/
     _("phase1").style.display = "none";
     _("phase2").style.display = "block";
-    _("progressBar").value = 66;
-    _("status").innerHTML = "Phase 2 of 3";
-  } else {
+    _("progressBar").value = 32;
+    _("status").innerHTML = "Phase 2 of 6";
+  /*} else {
       alert("Please fill in the fields"); 
-  }
+  }*/
 }
 function processPhase2(){
-    degree=_("degree").value;
+    /*degree=_("degree").value;
     school=_("school").value;
     fos=_("fos").value;
     study_city=_("study_city").value;
@@ -39,41 +39,78 @@ function processPhase2(){
     edu_from_year_time=_("edu_year_from").value;
     edu_month_to_time=_("edu_month_to").value;
     edu_year_to_time=_("edu_year_to").value;
-  if(degree.length > 0 && school.length > 0 && fos.length > 0 && study_city.length > 0 && edu_month_from != "0" && edu_year_from != "0" && edu_month_to != "0" && edu_year_to != "0"){
+  if(degree.length > 0 && school.length > 0 && fos.length > 0 && study_city.length > 0 && edu_month_from != "0" && edu_year_from != "0" && edu_month_to != "0" && edu_year_to != "0"){*/
     _("phase2").style.display = "none";
     _("phase3").style.display = "block";
-    _("progressBar").value = 100;
-    _("status").innerHTML = "Phase 3 of 3";
-  } else {
+    _("progressBar").value = 48;
+    _("status").innerHTML = "Phase 3 of 6";
+  /*} else {
       alert("Please fill in the fields"); 
-  }
+  }*/
+}
+function processPhase3(){
+    _("phase3").style.display = "none";
+    _("phase4").style.display = "block";
+    _("progressBar").value = 64;
+    _("status").innerHTML = "Phase 4 of 6";
+}
+function processPhase4(){
+    _("phase4").style.display = "none";
+    _("phase5").style.display = "block";
+    _("progressBar").value = 80;
+    _("status").innerHTML = "Phase 5 of 6";
+}
+function processPhase5(){
+    _("phase5").style.display = "none";
+    _("phase6").style.display = "block";
+    _("progressBar").value = 100;
+    _("status").innerHTML = "Phase 6 of 6";
 }
 function backPhase2(){
     _("phase2").style.display = "none";
     _("phase1").style.display = "block";
-    _("progressBar").value = 33;
-    _("status").innerHTML = "Phase 1 of 3";
+    _("progressBar").value = 16;
+    _("status").innerHTML = "Phase 1 of 6";
 }
 function backPhase3(){
     _("phase3").style.display = "none";
     _("phase2").style.display = "block";
-    _("progressBar").value = 66;
-    _("status").innerHTML = "Phase 2 of 3";
+    _("progressBar").value = 32;
+    _("status").innerHTML = "Phase 2 of 6";
 }
+function backPhase4(){
+    _("phase4").style.display = "none";
+    _("phase3").style.display = "block";
+    _("progressBar").value = 48;
+    _("status").innerHTML = "Phase 3 of 6";
+}
+function backPhase5(){
+    _("phase5").style.display = "none";
+    _("phase4").style.display = "block";
+    _("progressBar").value = 64;
+    _("status").innerHTML = "Phase 4 of 6";
+}
+function backPhase6(){
+    _("phase6").style.display = "none";
+    _("phase5").style.display = "block";
+    _("progressBar").value = 80;
+    _("status").innerHTML = "Phase 5 of 6";
+}
+
 function submitForm(){
-    job_title=_("job_title").value;
+   /* job_title=_("job_title").value;
     company=_("company").value;
     work_city=_("work_city").value;
     work_month=_("work_month").value;
     work_year=_("work_year").value;
-    if (job_title.length>0 && company.length>0 && work_city.length>0 && work_month!=="0" &&work_year!=="0") {
+    if (job_title.length>0 && company.length>0 && work_city.length>0 && work_month!="0" &&work_year!="0") {*/
         _("multiphase").method = "post";
         _("multiphase").action = "my_parser.php";
         _("multiphase").submit();
-    } 
+    /*} 
     else {
         alert("Please fill in the fields"); 
-    }
+    }*/
     
 }
 </script>
@@ -84,11 +121,12 @@ function submitForm(){
 <div id="login">
   <div id="triangle"></div>
   <h1>Create Resume</h1>
-  <progress id="progressBar" value="33" max="100" style="width:250px;"></progress>
-  <h3 id="status">Phase 1 of 3</h3>
+  <progress id="progressBar" value="16" max="100" style="width:250px;"></progress>
+  <h3 id="status">Phase 1 of 6</h3>
   <form id="multiphase" onsubmit="return false">
+    <!-- start phase1 -->
     <div id="phase1">
-        <h3>Basics</h3>
+        <h3>Personal Information</h3>
         <div>
         <p>First Name</p>
         <input id="first_name" name="first_name"/>
@@ -100,61 +138,80 @@ function submitForm(){
         </div>
 
         <div>
+        <p>Father Name</p>
+        <input id="father_name" name="father_name"/>
+        </div>
+
+        <div>
+        <p>Date of Birth</p>
+        <input type="date" id="birth_date" name="birth_date"/>
+        </div>
+
+        <div>
+        <p>Nationality</p>
+        <input id="nationality" name="nationality"/>
+        </div>
+
+        <div>
+        <p>Marital State</p>
+        <input type="radio" id="marital_state" name="marital_state" value="single" > Single<br>
+        <input type="radio" id="marital_state" name="marital_state" value="married" > Married<br>
+        </div>
+
+        <div>
+        <p>Gender</p>
+        <input type="radio" id="gender" name="gender" value="male" > Male<br>
+        <input type="radio" id="gender" name="gender" value="female" > Female<br>        
+        </div>
+
+        <div>
+        <p>Country</p>
+        <input id="country" name="country"/>
+        </div>
+
+        <div>
         <p>City</p>
         <input id="city" name="city"/>
         </div>
 
-        <h3>Contact Information</h3>
+        <div>
+        <p>Address</p>
+        <input id="address" name="address"/>
+        </div>
+
         <div>
         <p>Email</p>
         <input type="email" id="email" name="email"/>
         </div>
 
         <div>
-        <p>Phone Number (optional)</p>
+        <p>Phone Number</p>
         <input id="phone" name="phone"/>
-        </div>
-
-        <h3>Resume Privacy Setting</h3>
-        <div>
-        <input type="radio" id="gender" name="gender" value="public" checked="checked"> Public<br>
-        <p><span>Your resume will be visible to anyone, in accordance with our terms. Your phone number and email address are only provided to employers you apply or respond to. Your street address is visible only to you.</span></p>
-        </div>
-        <div>
-        <input type="radio" id="gender" name="gender" value="private"> Private<br>
-        <p><span>Your resume is not visible. Employers cannot find your resume, but you can attach it when you apply to a job.</span></p>
         </div>
 
         <button onclick="processPhase1()">Next</button>
     </div>
-
+    <!-- end phase1 -->
+  
+    <!-- start phase2 -->  
     <div id="phase2">
         <h3>Education</h3>
         <div>
-        <p>Degree</p>
-        <input id="degree" name="degree"/>
-        <p>e.g. BA, BS, JD, PhD.</p>
+        <p>Certificate Name</p>
+        <input id="cert_name" name="cert_name"/>
         </div>
 
         <div>
-        <p>School</p>
-        <input id="school" name="school"/>
-        </div>
+        <p>Date of Grants</p>
+        <select id="grants_day" name="grants_day">
+        <option value="0">Day</option>
+        <?php 
+        for ($i = 1; $i <= 31; $i++) {
+        echo "<option value='.$i.'>$i</option>";
+        }?>
+        </select>
 
-        <div>
-        <p>Field of Study</p>
-        <input id="fos" name="fos"/>
-        <p>e.g. Biology, Computer Science, Intellectual Property, Nursing, Psychology.</p>
-        </div>
-
-        <div>
-        <p>Study City</p>
-        <input id="study_city" name="study_city"/>
-        </div>
-
-        <div>
-        <p>Time Period</p>
-        <select id=edu_month_from name=edu_month_from>
+        <select id="grants_month" name="grants_month">
         <option value="0">Month</option>
         <option value="1">January</option>
         <option value="2">February</option>
@@ -171,7 +228,7 @@ function submitForm(){
         </select>
 
         <?php $year=2019;?>
-        <select id=edu_year_from name=edu_year_from>
+        <select id=grants_year name=grants_year>
         <option value="0">Year</option>
         <?php 
         for ($i = 0; $i <= 117; $i++) {
@@ -179,62 +236,48 @@ function submitForm(){
         echo "<option value='.$year.'>$year</option>";
         }?>
         </select>
-
-        <span>to</span>
-
-        <select id=edu_month_to name=edu_month_to>
-        <option value="0">Month</option>
-        <option value="1">January</option>
-        <option value="2">February</option>
-        <option value="3">March</option>
-        <option value="4">April</option>
-        <option value="5">May</option>
-        <option value="6">June</option>
-        <option value="7">July</option>
-        <option value="8">August</option>
-        <option value="9">September</option>
-        <option value="10">October</option>
-        <option value="11">November</option>
-        <option value="12">December</option>
-        </select>
-
-        <?php $year1=2019;?>
-        <select select id=edu_year_to name=edu_year_to>
-        <option value="0">Year</option>
-        <?php 
-        for ($x = 0; $x <= 117; $x++) {
-        $year1=$year1-1;
-        echo "<option value='.$year1.'>$year1</option>";
-        }?>
-        </select>
-
-        <span>Current students: enter your expected graduation year</span>
         </div>
+
+        <div>
+        <p>Donor</p>
+        <input id="donor" name="donor"/>
+        </div>
+
+        <div>
+        <p>The Rate (optional)</p>
+        <input id="rate" name="rate"/>
+        </div>
+
+        
         <button onclick="processPhase2()">Next</button>
         <button onclick="backPhase2()">Previous</button>
     </div>
+    <!-- end phase2 -->
 
-    
+    <!-- start phase3 --> 
     <div id="phase3">
         <h3>Work Experience</h3>
         <div>
-        <p>Job Title</p>
-        <input id="job_title" name="job_title"/>
+        <p>Company Name</p>
+        <input id="company_name" name="company_name"/>
         </div>
 
         <div>
-        <p>Company</p>
-        <input id="company" name="company"/>
-        </div>
-
-        <div>
-        <p>City</p>
-        <input id="work_city" name="work_city"/>
+        <p>Job Position</p>
+        <input id="job_pos" name="job_pos"/>
         </div>
 
         <div>
         <p>Time Period</p>
-        <select id="work_month" name="work_month">
+        <select id="time_day_from" name="time_day_from">
+        <option value="0">Day</option>
+        <?php 
+        for ($i = 1; $i <= 31; $i++) {
+        echo "<option value='.$i.'>$i</option>";
+        }?>
+        </select>
+
+        <select id="time_month_from" name="time_month_from">
         <option value="0">Month</option>
         <option value="1">January</option>
         <option value="2">February</option>
@@ -251,7 +294,7 @@ function submitForm(){
         </select>
 
         <?php $year2=2019;?>
-        <select id="work_year" name="work_year">
+        <select id="time_year_from" name="time_year_from">
         <option value="0">Year</option>
         <?php 
         for ($y = 0; $y <= 117; $y++) {
@@ -259,28 +302,138 @@ function submitForm(){
         echo "<option value='.$year.'>$year2</option>";
         }?>
         </select>
-        <span>to</span>
-        <b>Present</b>
+
+        <p><span>to</span></p>
+
+        <select id="time_day_to" name="time_day_to">
+        <option value="0">Day</option>
+        <?php 
+        for ($i = 1; $i <= 31; $i++) {
+        echo "<option value='.$i.'>$i</option>";
+        }?>
+        </select>
+
+        <select id="time_month_to" name="time_month_to">
+        <option value="0">Month</option>
+        <option value="1">January</option>
+        <option value="2">February</option>
+        <option value="3">March</option>
+        <option value="4">April</option>
+        <option value="5">May</option>
+        <option value="6">June</option>
+        <option value="7">July</option>
+        <option value="8">August</option>
+        <option value="9">September</option>
+        <option value="10">October</option>
+        <option value="11">November</option>
+        <option value="12">December</option>
+        </select>
+
+        <?php $year2=2019;?>
+        <select id="time_year_to" name="time_year_to">
+        <option value="0">Year</option>
+        <?php 
+        for ($y = 0; $y <= 117; $y++) {
+        $year2=$year2-1;
+        echo "<option value='.$year.'>$year2</option>";
+        }?>
+        </select>
         </div>
 
         <div>
-        <p>Description</p>
-        <textarea name="description"></textarea>
-        <p><span>Describe your position and any significant accomplishments.</span></p>
+        <p>Main activities and responsibilities</p>
+        <textarea id="activity" name="activity"></textarea>
         </div>
-        <button onclick="submitForm()">Submit Data</button>
+
+        <div>
+        <p>Significant Accomplishments</p>
+        <textarea id="Accomplishments" name="Accomplishments"></textarea>
+        </div>
+
+        <button onclick="processPhase3()">Next</button>
         <button onclick="backPhase3()">Previous</button>
     </div>
+    <!-- end phase3 -->
 
+    <!-- start phase4 -->
+    <div id="phase4">
+        <h3>Personal Skills</h3>
+        <div>
+        <p>Skill Name</p>
+        <input id="skill_name" name="skill_name"/>
+        </div>
+
+        <div>
+        <p>Years of experience</p>
+        <select id="year_exp" name="year_exp">
+        <option value="">choose ...</option>
+        <option value="less than 1 year">less than 1 year</option>
+        <option value="1 year">1 year</option>
+        <option value="more than 1 year">more than 1 year</option>
+        <option value="2 year">2 year</option>
+        <option value="more than 2 year">more than 2 year</option>
+        <option value="3 year">3 year</option>
+        <option value="more than 3 year">more than 3 year</option>
+        </select>
+        </div>
+
+        <button onclick="processPhase4()">Next</button>
+        <button onclick="backPhase4()">Previous</button>
+    </div>
+    <!-- end phase4 -->
+
+    <!-- start phase5 -->
+    <div id="phase5">
+        <h3>Personal Interests</h3>
+        <div>
+        <p>Interest Name</p>
+        <input id="interest_name" name="company_name"/>
+        </div>
+
+        <div>
+        <p>Degree of interest</p>
+        <select id="interest_degree" name="interest_degree">
+        <option value="">choose ...</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        </select>
+        </div>
+
+        <button onclick="processPhase5()">Next</button>
+        <button onclick="backPhase5()">Previous</button>
+    </div>
+    <!-- end phase5 -->
+
+    <!-- start phase6 -->
+    <div id="phase6">
+        <h3>References & Referees</h3>
+        <div>
+        <p>Person/Company/Organization Name</p>
+        <input id="ref_name" name="ref_name"/>
+        </div>
+
+        <div>
+        <p>Phone Number (Optional)</p>
+        <input id="ref_phone" name="ref_phone"/>
+        </div>
+
+        <div>
+        <p>Email (Optional)</p>
+        <input id="ref_email" name="ref_email"/>
+        </div>
+
+        <button onclick="submit()">Save</button>
+        <button onclick="backPhase6()">Previous</button>
+    </div>
+    <!-- end phase6 -->
   </form>
 </div>
 
   <script src="<?php echo base_url('http://codepen.io/assets/libs/fullpage/jquery.js')?>"></script>
-
   <script src="<?php echo base_url('assets/bootstrap/js/index')?>"></script>
-
-
-  
 
 </body>
 
