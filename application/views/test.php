946166243e6106674687e2f306fccf04f71f9a6d
Walaa1994 <walaa.albaghdadi@hotmail.com>
2018-04-25 21:35:54 +0300

    view for testing

---
+++ application/views/test.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html class="no-js" lang="en-US">
+
+
+
+
+
+<body>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="header-nav-wrapper">
+				
+				<div class="primary-nav-wrapper">
+					
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	<div class="hero-bg">
+					<div class="container">
+						<div class="row">
+							
+								
+							
+							</div>
+					
+						</div>
+						
+					</div>
+				</div>
+
+	
+	
+	
+	
+	
+	
+</body>
+
+</html>
