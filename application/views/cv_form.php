a6538e2fb901e0aded9a8fa1091580e3cf962242
Walaa1994 <walaa.albaghdadi@hotmail.com>
2018-04-25 21:36:39 +0300

    modify view's style

--- application/views/cv_form.php
+++ application/views/cv_form.php
@@ -2,12 +2,10 @@
 <html>
 
 <head>
-
+ <link href="<?php echo base_url();?>/assets/css/cv.css" rel='stylesheet' type='text/css' />
   <meta charset="UTF-8">
   <title>CV Form</title>  
-  <link href="<?php echo base_url();?>/assets/css/style.css" rel='stylesheet' type='text/css' />
   <style>
-form#multiphase{ border:#000 1px solid; padding:24px; width:350px; }
 form#multiphase > #phase2, #phase3, #phase4, #phase5, #phase6{ display:none; }
 </style>
   <script>
@@ -118,65 +116,65 @@ function submitForm(){
 
 <body>
 
-<div id="login">
+<div id="form">
   <div id="triangle"></div>
-  <h1>Create Resume</h1>
-  <progress id="progressBar" value="16" max="100" style="width:250px;"></progress>
-  <h3 id="status">Phase 1 of 6</h3>
+  
   <form id="multiphase" onsubmit="return false">
+      <progress id="progressBar" value="16" max="100" style="width:250px;"></progress>
+      <h3 id="status">Phase 1 of 6</h3>
     <!-- start phase1 -->
     <div id="phase1">
         <h3>Personal Information</h3>
         <div>
         <p>First Name</p>
-        <input id="first_name" name="first_name"/>
+        <input type="first_name" id="first_name" name="first_name"/>
         </div>
 
         <div>
         <p>Last Name</p>
-        <input id="last_name" name="last_name"/>
+        <input type="last_name" id="last_name" name="last_name"/>
         </div>
 
         <div>
         <p>Father Name</p>
-        <input id="father_name" name="father_name"/>
+        <input  type="father_name" id="father_name" name="father_name"/>
         </div>
 
         <div>
         <p>Date of Birth</p>
-        <input type="date" id="birth_date" name="birth_date"/>
+        <input type="birth_date" id="birth_date" name="birth_date"/>
         </div>
 
         <div>
         <p>Nationality</p>
-        <input id="nationality" name="nationality"/>
+        <input type="nationality" id="nationality" name="nationality"/>
         </div>
 
         <div>
-        <p>Marital State</p>
+        <p id="radio_marital">Marital State</p>
         <input type="radio" id="marital_state" name="marital_state" value="single" > Single<br>
         <input type="radio" id="marital_state" name="marital_state" value="married" > Married<br>
         </div>
 
         <div>
-        <p>Gender</p>
+        <p id="radio_gender">Gender</p>
         <input type="radio" id="gender" name="gender" value="male" > Male<br>
         <input type="radio" id="gender" name="gender" value="female" > Female<br>        
         </div>
 
         <div>
         <p>Country</p>
-        <input id="country" name="country"/>
+        <input type="country" id="country" name="country"/>
         </div>
 
         <div>
         <p>City</p>
-        <input id="city" name="city"/>
+        <input type="city" id="city" name="city"/>
         </div>
 
         <div>
         <p>Address</p>
-        <input id="address" name="address"/>
+        <input type="address" id="address" name="address"/>
         </div>
 
         <div>
@@ -186,10 +184,10 @@ function submitForm(){
 
         <div>
         <p>Phone Number</p>
-        <input id="phone" name="phone"/>
+        <input type="phone" id="phone" name="phone"/>
         </div>
 
-        <button onclick="processPhase1()">Next</button>
+        <button id="next" onclick="processPhase1()">Next</button>
     </div>
     <!-- end phase1 -->
   
@@ -198,12 +196,12 @@ function submitForm(){
         <h3>Education</h3>
         <div>
         <p>Certificate Name</p>
-        <input id="cert_name" name="cert_name"/>
+        <input type="cert_name" id="cert_name" name="cert_name"/>
         </div>
 
         <div>
         <p>Date of Grants</p>
-        <select id="grants_day" name="grants_day">
+        <select id="select" name="grants_day">
         <option value="0">Day</option>
         <?php 
         for ($i = 1; $i <= 31; $i++) {
@@ -211,7 +209,7 @@ function submitForm(){
         }?>
         </select>
 
-        <select id="grants_month" name="grants_month">
+        <select id="select" name="grants_month">
         <option value="0">Month</option>
         <option value="1">January</option>
         <option value="2">February</option>
@@ -228,7 +226,7 @@ function submitForm(){
         </select>
 
         <?php $year=2019;?>
-        <select id=grants_year name=grants_year>
+        <select id=select name=grants_year>
         <option value="0">Year</option>
         <?php 
         for ($i = 0; $i <= 117; $i++) {
@@ -240,17 +238,17 @@ function submitForm(){
 
         <div>
         <p>Donor</p>
-        <input id="donor" name="donor"/>
+        <input type="donor" id="donor" name="donor"/>
         </div>
 
         <div>
         <p>The Rate (optional)</p>
-        <input id="rate" name="rate"/>
+        <input type="rate" id="rate" name="rate"/>
         </div>
 
         
-        <button onclick="processPhase2()">Next</button>
-        <button onclick="backPhase2()">Previous</button>
+        <button id="next" onclick="processPhase2()">Next</button>
+        <button id="previous" onclick="backPhase2()">Previous</button>
     </div>
     <!-- end phase2 -->
 
@@ -259,17 +257,18 @@ function submitForm(){
         <h3>Work Experience</h3>
         <div>
         <p>Company Name</p>
-        <input id="company_name" name="company_name"/>
+        <input type="company_name" id="company_name" name="company_name"/>
         </div>
 
         <div>
         <p>Job Position</p>
-        <input id="job_pos" name="job_pos"/>
+        <input type="job_pos" id="job_pos" name="job_pos"/>
         </div>
 
         <div>
         <p>Time Period</p>
-        <select id="time_day_from" name="time_day_from">
+        <p><span>from</span></p>
+        <select id="select" name="time_day_from">
         <option value="0">Day</option>
         <?php 
         for ($i = 1; $i <= 31; $i++) {
@@ -277,7 +276,7 @@ function submitForm(){
         }?>
         </select>
 
-        <select id="time_month_from" name="time_month_from">
+        <select id="select" name="time_month_from">
         <option value="0">Month</option>
         <option value="1">January</option>
         <option value="2">February</option>
@@ -294,7 +293,7 @@ function submitForm(){
         </select>
 
         <?php $year2=2019;?>
-        <select id="time_year_from" name="time_year_from">
+        <select id="select" name="time_year_from">
         <option value="0">Year</option>
         <?php 
         for ($y = 0; $y <= 117; $y++) {
@@ -305,7 +304,7 @@ function submitForm(){
 
         <p><span>to</span></p>
 
-        <select id="time_day_to" name="time_day_to">
+        <select id="select" name="time_day_to">
         <option value="0">Day</option>
         <?php 
         for ($i = 1; $i <= 31; $i++) {
@@ -313,7 +312,7 @@ function submitForm(){
         }?>
         </select>
 
-        <select id="time_month_to" name="time_month_to">
+        <select id="select" name="time_month_to">
         <option value="0">Month</option>
         <option value="1">January</option>
         <option value="2">February</option>
@@ -330,7 +329,7 @@ function submitForm(){
         </select>
 
         <?php $year2=2019;?>
-        <select id="time_year_to" name="time_year_to">
+        <select id="select" name="time_year_to">
         <option value="0">Year</option>
         <?php 
         for ($y = 0; $y <= 117; $y++) {
@@ -350,8 +349,8 @@ function submitForm(){
         <textarea id="Accomplishments" name="Accomplishments"></textarea>
         </div>
 
-        <button onclick="processPhase3()">Next</button>
-        <button onclick="backPhase3()">Previous</button>
+        <button id="next" onclick="processPhase3()">Next</button>
+        <button id="previous" onclick="backPhase3()">Previous</button>
     </div>
     <!-- end phase3 -->
 
@@ -360,12 +359,12 @@ function submitForm(){
         <h3>Personal Skills</h3>
         <div>
         <p>Skill Name</p>
-        <input id="skill_name" name="skill_name"/>
+        <input type="skill_name" id="skill_name" name="skill_name"/>
         </div>
 
         <div>
         <p>Years of experience</p>
-        <select id="year_exp" name="year_exp">
+        <select id="select" name="year_exp">
         <option value="">choose ...</option>
         <option value="less than 1 year">less than 1 year</option>
         <option value="1 year">1 year</option>
@@ -377,8 +376,8 @@ function submitForm(){
         </select>
         </div>
 
-        <button onclick="processPhase4()">Next</button>
-        <button onclick="backPhase4()">Previous</button>
+        <button id="next" onclick="processPhase4()">Next</button>
+        <button id="previous" onclick="backPhase4()">Previous</button>
     </div>
     <!-- end phase4 -->
 
@@ -387,12 +386,12 @@ function submitForm(){
         <h3>Personal Interests</h3>
         <div>
         <p>Interest Name</p>
-        <input id="interest_name" name="company_name"/>
+        <input type="interest_name" id="interest_name" name="company_name"/>
         </div>
 
         <div>
         <p>Degree of interest</p>
-        <select id="interest_degree" name="interest_degree">
+        <select id="select" name="interest_degree">
         <option value="">choose ...</option>
         <option value="1">1</option>
         <option value="2">2</option>
@@ -402,8 +401,8 @@ function submitForm(){
         </select>
         </div>
 
-        <button onclick="processPhase5()">Next</button>
-        <button onclick="backPhase5()">Previous</button>
+        <button id="next" onclick="processPhase5()">Next</button>
+        <button id="previous" onclick="backPhase5()">Previous</button>
     </div>
     <!-- end phase5 -->
 
@@ -412,21 +411,21 @@ function submitForm(){
         <h3>References & Referees</h3>
         <div>
         <p>Person/Company/Organization Name</p>
-        <input id="ref_name" name="ref_name"/>
+        <input type="ref_name" id="ref_name" name="ref_name"/>
         </div>
 
         <div>
         <p>Phone Number (Optional)</p>
-        <input id="ref_phone" name="ref_phone"/>
+        <input type="ref_phone" id="ref_phone" name="ref_phone"/>
         </div>
 
         <div>
         <p>Email (Optional)</p>
-        <input id="ref_email" name="ref_email"/>
+        <input type="ref_email" id="ref_email" name="ref_email"/>
         </div>
 
-        <button onclick="submit()">Save</button>
-        <button onclick="backPhase6()">Previous</button>
+        <button id="next" onclick="submit()">Save</button>
+        <button id="previous" onclick="backPhase6()">Previous</button>
     </div>
     <!-- end phase6 -->
   </form>
