8f5cbb19ab6c31e8df6b7ccbe2599f29b16f8cbc
Walaa1994 <walaa.albaghdadi@hotmail.com>
2018-04-25 21:37:04 +0300

    modify view's style

--- application/views/company_form.php
+++ application/views/company_form.php
@@ -2,35 +2,30 @@
 <html>
 
 <head>
-
+  <link href="<?php echo base_url();?>/assets/css/cv.css" rel='stylesheet' type='text/css' />
   <meta charset="UTF-8">
-
   <title>Company Form</title>
-
-    
-    <link href="<?php echo base_url();?>/assets/css/style.css" rel='stylesheet' type='text/css' />
-
 </head>
 
 <body>
-
-<div id="login">
+<div id="form">
   <div id="triangle"></div>
-  <h1>Add Company</h1>
-  <form action="<?php echo base_url();?>index.php/login/login_validation" method="post">
+  <form id="multiphase" action="<?php echo base_url();?>index.php/login/login_validation" method="post">
     
     <div>
       <p>Company Name (Arabic)</p>
-      <input id="ar_com_name" name="ar_com_name"/>
+      <input type="ar_com_name" id="ar_com_name" name="ar_com_name"/>
     </div>
+
     <div>
       <p>Company Name (English)</p>
-      <input id="en_com_name" name="en_com_name"/>
+      <input type="en_com_name" id="en_com_name" name="en_com_name"/>
     </div>
+
     <!--professional field need modify to select -->
      <div>
       <p>Professional Field of The Company</p>
-      <select id="com_field" name="com_field">
+      <select id="select" name="com_field">
         <option value="0">Choose ...</option>
         <option value="designing">Designing</option>
         <option value="information technology">Information Technology</option>
@@ -47,10 +42,11 @@
         <option value="secretarial">Secretarial</option>
         </select>
     </div>
+
     <div>
       <?php $year=2019;?>
       <p>The Year of Foundation</p>
-      <select id=com_year name=com_year>
+      <select id=select name=com_year>
       <option value="0">Year</option>
       <?php 
       for ($i = 0; $i <= 117; $i++) {
@@ -59,33 +55,37 @@
       }?>
       </select>
     </div>
+
     <div>
       <p>The Owner of Company</p>
-      <input id="com_owner" name="com_owner"/>
+      <input type="com_owner" id="com_owner" name="com_owner"/>
     </div>
+
     <div>
       <p>City</p>
-      <input id="com_city" name="com_city"/>
+      <input type="com_city" id="com_city" name="com_city"/>
     </div>
+
     <div>
       <p>Governorate</p>
-      <input id="com_governorate" name="com_governorate"/>
+      <input type="com_governorate" id="com_governorate" name="com_governorate"/>
     </div>
+
     <div>
       <p>Address</p>
-      <input id="com_address" name="com_address"/>
+      <input type="com_address" id="com_address" name="com_address"/>
     </div>
     <div>
       <p>phone</p>
-      <input id="com_phone" name="com_phone"/>
+      <input type="com_phone" id="com_phone" name="com_phone"/>
     </div>
     <div>
       <p>Email</p>
-      <input id="com_email" name="com_email"/>
+      <input type="com_email" id="com_email" name="com_email"/>
     </div>
     <div>
       <p>Web Site</p>
-      <input id="com_website" name="com_website"/>
+      <input type="com_website" id="com_website" name="com_website"/>
     </div>
     <input type="submit" value="Submit" />
   </form>
