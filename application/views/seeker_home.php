5ea150676148daeacb8a72d67649cf5d4d30b1b0
Hamida AboRshed <Hamida.aborshed@gmail.com>
2018-07-21 05:10:50 +0300

    upload page, search advance, bigfve

--- application/views/seeker_home.php
+++ application/views/seeker_home.php
@@ -2,7 +2,15 @@
                       
                       $(document).ready(function () {
                           $(".mdb-select").formSelect();
-                        });
+                      });
+
+                          function taggle(){
+                            debugger;
+                            if($("#advance_search").css("display")=="none")
+                              $("#advance_search").css("display","inline-flex");
+                            else
+                              $("#advance_search").css("display","none");
+                          }
                     </script>
 
                       <!--<form class="form-inline">
@@ -11,48 +19,56 @@
                       </div>
                     </form>
                     -->
-                     <div class="col-md-10 mb-4">
-                    <div class="md-form">
+                 <div class="col-md-10 mb-4">
+                    <div class="md-form" style="display: inline-flex;width: 100%;">
                         <input type="text" id="job_title" class="form-control">
                         <label for="job_title" class="">Job Title</label>
+                        <div>
+                          <a class="fa-lg p-2 m-2">
+                            <i class="fa fa-search"> </i>
+                          </a>
+                          <a href="#" id="more" class="fa-lg p-2 m-2" onclick="taggle();">
+                           <i class="fa fa-bars" style="color: #000;"> </i>
+                          </a>
+                        </div>
+
                     </div>
-                </div
-
-
-                    <div>
+                    <div class="row" id="advance_search" style="margin-top: 0px;display: none;">
+                  <div class="col-md-3 mb-3">
                     <select class="mdb-select colorful-select dropdown-primary">
                      <option>Full Time</option>
                      <option>Part Time</option>
                      </select>
+                   </div>
+                   <div class="col-md-3 mb-3">
                       <select class="mdb-select colorful-select dropdown-primary">
-                     <option>Employee</option>
-                     <option>Contractor</option>
-                     <option>inter</option>
+                       <option>Employee</option>
+                       <option>Contractor</option>
+                       <option>inter</option>
                      </select>
-
-                  
-                </div>
-                <div class="col-md-10 mb-4">
+                   </div>
+                  <div class="col-md-3 mb-3">
                     <div class="md-form">
                         <input type="text" id="salary" class="form-control">
                         <label for="salary" class="">Salary</label>
                     </div>
-                </div>
-                <div class="col-md-10 mb-4">
+                  </div>
+                  <div class="col-md-3 mb-3">
                     <div class="md-form">
                         <input type="text" id="locality" class="form-control">
                         <label for="locality" class="">Locality</label>
                     </div>
+                  </div>
                 </div>
-
-                    <div class="mdl-card__actions  mdl-card--border">
-                      <a href="#" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent">
-                         search
-                      </a>
-                    </div>
+                </div>
+                <!-- <div class="mdl-card__actions  mdl-card--border">
+                  <a href="#" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent">
+                     search
+                  </a>
+                </div> -->
+                
              <div class="row">
                 <div class="col-md-6">
-
                     <!-- Card -->
                     <div class="card card-image mb-3" style="background-image: url('https://mdbootstrap.com/img/Photos/Categories/Components/img(10).jpg');">
 
