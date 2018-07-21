27b7e1ee548f59d27c54863841e9654ee5efe30b
EnasAlHalabi <enas.alhalabi.95@gmail.com>
2018-07-21 22:08:32 +0300

    cv_pdf
    downloadCV in home and cv view

--- application/views/newCV.php
+++ application/views/newCV.php
@@ -1,22 +1,4 @@
-<div class="mdl-grid site-max-width">
-	<div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp welcome-card portfolio-card">
-    <div class="mdl-card__title">
-      <h2 class="mdl-card__title-text">Are you already have CV?</h2>
-    </div>
-    <div class="mdl-card__supporting-text">
-      you can upload your cv and all data extract and re-stract with professional cv.
-    </div>
-    <div class="mdl-card__actions mdl-card--border">
-      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="<?php echo(site_url('seeker/uploadcv'));?>" target="_blank">
-        Upload
-      </a>
-       /
-      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="<?php echo(site_url('seeker/cvForm'));?>" target="_blank">
-        Create Your CV.
-      </a>
-    </div>
-  </div>
-</div>
+
 
 <section class="section--center mdl-grid site-max-width">
   <header class="section__play-btn mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone mdl-color--teal-100 mdl-color-text--white  mdl-shadow--4dp">
@@ -34,13 +16,9 @@
            echo $Email; echo "<br>";
            echo "Address : ".$Country."-".$City."-".$Street; ?>
     </div>
-    <div class="mdl-card__actions  mdl-card--border">
-      <a href="#" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent">
-        Find Job
-      </a>
-    </div>
   </div>
 </section>
+
 <section class="section--center mdl-grid site-max-width">
     <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
         <div class="mdl-card__media"  >
@@ -119,56 +97,8 @@
     </div>
 </section>
 
-<section class="section--center mdl-grid site-max-width homepage-portfolio">
-    <a class="mdl-button mdl-button--raised mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="http://localhost/cv/index.php/home/downloadCV">
-      Download CV
-    </a>
-</section>
+<?php class myPDF extends FPDF{
 
-<div class="homepage-footer">
-  <section class="mdl-grid site-max-width">
-      <div class="mdl-cell mdl-card mdl-cell--8-col mdl-cell--4-col-tablet  mdl-shadow--4dp portfolio-card">
-        <div class="mdl-card__title">
-            <h2 class="mdl-card__title-text">Your Personal test result</h2>
-        </div>
-        <ul class="demo-list-three mdl-list">
-            <li class="mdl-list__item mdl-list__item--three-line">
-              <span class="mdl-list__item-primary-content">
-                <i class="material-icons mdl-list__item-avatar">person</i>
-                <span>Amazing people, always ready to help!</span>
-                <span class="mdl-list__item-text-body">
-                  Bryan Cranston, CEO, Amazing.com
-                </span>
-              </span>
-            </li>
-            <li class="mdl-list__item mdl-list__item--three-line">
-              <span class="mdl-list__item-primary-content">
-                <i class="material-icons  mdl-list__item-avatar">person</i>
-                <span>Awesome work, they can do almost anything..</span>
-                <span class="mdl-list__item-text-body">
-                Aaron Paul, Marketing Lead, Awesome.com
-                </span>
-              </span>
-            </li>
-        </ul>
-      </div>
-      <div class="demo-card-event mdl-cell mdl-card mdl-shadow--4dp event-card portfolio-card">
-        <div class="mdl-card__title mdl-card--expand">
-          <h4>
-            Openness : 20%<br>
-            Conscientiousness : 40% <br>
-            Extraversion : 30%<br>
-            Agreeableness : 79%<br>
-            Neuroticism : 70%
-          </h4>
-        </div>
-        <div class="mdl-card__actions mdl-card--border">
-          <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="<?php echo(site_url('FacebookLogin/index')); ?>">
-            Personal Test
-          </a>
-          <div class="mdl-layout-spacer"></div>
-          <i class="fa fa-facebook-official" aria-hidden="true"></i>
-        </div>
-      </div>
-  </section>
-  </div>
+$pdf = new myPDF(); 
+
+?>
