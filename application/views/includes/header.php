245719dcec639dd77b1c3b29d69e7183aab1b6bf
batoulalsharef <batoulalsharef386@gmail.com>
2018-07-24 01:56:46 +0300

    add css style edite seeker controller for job type, edite announcement for image,

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,10 +4,13 @@
     <title>CV Star Soft</title>
     <meta name="description" content="Demo of Material design portfolio template by TemplateFlip.com."/>
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;amp;lang=en" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   <!--  <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;amp;lang=en" rel="stylesheet"> -->
+    <link href="<?php echo base_url();?>/assets/css/Roboto.css" rel="stylesheet">
+    
+   <!--  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+    <link href="<?php echo base_url();?>/assets/css/Material-Icons.css" rel="stylesheet">
     <!-- the first-->
-   <link href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" rel="stylesheet">
+     <!-- <link href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" rel="stylesheet">  -->
     <link href="<?php echo base_url();?>/assets/styles/stylesmaterial.indigo-pink.min.css" rel="stylesheet">
      <!-- end first-->
 
@@ -18,15 +21,18 @@
 
     <!-- Font Awesome -->
     <!-- second -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link  href="<?php echo base_url();?>/assets/styles/font-awesome.min.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link  href="<?php echo base_url();?>/assets/icons/css/font-awesome.min.css" rel="stylesheet">
+
       <!-- second end -->
+
     <!-- Bootstrap core CSS -->
    <link href="<?php echo base_url();?>/assets/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
    <link href="<?php echo base_url();?>/assets/css/mdb.min.css" rel="stylesheet">
 
     <link href="<?php echo base_url();?>/assets/css/compiled-4.5.7.min.css" rel="stylesheet">
+    <link  href="<?php echo base_url();?>/assets/css/material.min.css" rel="stylesheet">
 
     <!-- datetime picker -->
     <link href="<?php echo base_url();?>/assets/css/bootstrap-material-datetimepicker.css" rel="stylesheet">
