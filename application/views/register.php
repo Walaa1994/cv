f8f7b23cc29f3d8b8f00a88633f15e3c5112687f
batoulalsharef <batoulalsharef386@gmail.com>
2018-07-28 03:18:10 +0300

    edite design login

--- application/views/register.php
+++ application/views/register.php
@@ -33,7 +33,7 @@
 
   
   <div class="limiter">
-    <div class="container-login100">
+    <div style="background-color:#D0DDE9" class="container-login100">
       <div class="wrap-login100">
         <div class="login100-form-title" style="background-image: url(<?php echo base_url();?>/assets/login/images/bg-01.jpg);">
           <span class="login100-form-title-1">
