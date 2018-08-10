de3cdbd31a8953daa0f4fc4ff0ee9c178cba68d3
EnasAlHalabi <enas.alhalabi.95@gmail.com>
2018-08-10 12:40:30 +0300

    cv pdf
    add 2 template for cv pdf

--- application/views/cvtcpdf.php
+++ application/views/cvtcpdf.php
@@ -50,134 +50,144 @@ $pdf->AddPage();
 $pdf->setFormDefaultProp(array('lineWidth'=>1, 'borderStyle'=>'solid', 'fillColor'=>array(255, 255, 200), 'strokeColor'=>array(255, 128, 128)));
 
 $pdf->SetFont('Times', 'BI', 18);
+$pdf->SetTextColor(77,77,77);
 $pdf->Cell(0, 5, $first_name.' '.$last_name, 0, 1, 'C');
 $pdf->Ln(10);
 
 $pdf->SetFont('helvetica', '', 12);
-
+$pdf->SetTextColor(0,0,0);
 //personal info
-$pdf->Cell(35, 5, 'Birthday:');
+$pdf->Cell(35, 5, 'Birthday');
 $pdf->Cell(35, 5, $birthday);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Nationality:');
+$pdf->Cell(35, 5, 'Nationality');
 $pdf->Cell(35, 5, $Nationality);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Marital Status:');
+$pdf->Cell(35, 5, 'Marital Status');
 $pdf->Cell(35, 5, $MaritalStatus);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Gender:');
+$pdf->Cell(35, 5, 'Gender');
 $pdf->Cell(35, 5, $gender);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Telephone:');
+$pdf->Cell(35, 5, 'Telephone');
 $pdf->Cell(35, 5, $Phone);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Email:');
+$pdf->Cell(35, 5, 'Email');
 $pdf->Cell(35, 5, $Email);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Country:');
+$pdf->Cell(35, 5, 'Country');
 $pdf->Cell(35, 5, $Country);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'City:');
+$pdf->Cell(35, 5, 'City');
 $pdf->Cell(35, 5, $City);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Street:');
+$pdf->Cell(35, 5, 'Street');
 $pdf->Cell(35, 5, $Street);
 $pdf->Ln(6);
 
 //Education
+$pdf->SetFont('Times', 'BI', 16);
 $pdf->Cell(0,5,'Education',0,0,'C');
 $pdf->Ln();
     foreach($eduMajor as $key => $value)
         {
-            //$pdf->SetFont('Times','',16);
+            $pdf->SetFont('helvetica','',12);
             //$pdf->SetTextColor(0,0,0);
-            $pdf->Cell(35,5,'Certificate :',0,0,'L');
+            $pdf->Cell(35,5,'Certificate ',0,0,'L');
             $pdf->Cell(35,5,$value,0,1,'L');
-            $pdf->Cell(35,5,'Specialization :',0,0,'L');
+            $pdf->Cell(35,5,'Specialization ',0,0,'L');
             $pdf->Cell(35,5,$eduMinor[$key],0,1,'L');
-            $pdf->Cell(35,5,'Education start date :',0,0,'L');
+            $pdf->Cell(35,5,'start date ',0,0,'L');
             $pdf->Cell(35,5,$eduStartDate[$key],0,1,'L');
-            $pdf->Cell(35,5,'Graduation date :',0,0,'L');
+            $pdf->Cell(35,5,'Graduation date ',0,0,'L');
             $pdf->Cell(35,5,$eduGradDate[$key],0,1,'L');
-            $pdf->Cell(35,5,'University :',0,0,'L');
+            $pdf->Cell(35,5,'University ',0,0,'L');
             $pdf->Cell(35,5,$studiedIn[$key],0,1,'L');
-            $pdf->Cell(35,5,'Degree :',0,0,'L');
+            $pdf->Cell(35,5,'Degree ',0,0,'L');
             $pdf->Cell(35,5,$degreeType[$key],0,1,'L');
             $pdf->Ln();
             $pdf->Ln();
         }
 
 //work experience
+$pdf->SetFont('Times', 'BI', 16);
 $pdf->Cell(0,5,'Work Experience',0,0,'C');
 $pdf->Ln();
     foreach($employedIn as $key => $value)
         {
-            $pdf->Cell(35,5,'Company :',0,0,'L');
+            $pdf->SetFont('helvetica','',12);
+            $pdf->Cell(35,5,'Company ',0,0,'L');
             $pdf->Cell(35,5,$value,0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'job Title :',0,0,'L');   
+            $pdf->Cell(35,5,'job Title ',0,0,'L');   
             $pdf->Cell(35,5,$jobTitle[$key],0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Start Date :',0,0,'L');
+            $pdf->Cell(35,5,'Start Date ',0,0,'L');
             $pdf->Cell(35,5,$startDate[$key],0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'End Date :',0,0,'L');
+            $pdf->Cell(35,5,'End Date ',0,0,'L');
             $pdf->Cell(35,5,$startDate[$key],0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Career Level :',0,0,'L');
+            $pdf->Cell(35,5,'Career Level ',0,0,'L');
             $pdf->Cell(35,5,$careerLevel[$key],0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Job Type :',0,0,'L');    
+            $pdf->Cell(35,5,'Job Type ',0,0,'L');    
             $pdf->Cell(35,5,$jobType[$key],0,0,'L');
             $pdf->Ln();
             $pdf->Ln();        
         }    
 
 //skills
+$pdf->SetFont('Times', 'BI', 16);
 $pdf->Cell(0,5,'Skills',0,0,'C');
 $pdf->Ln();
     foreach($skillName as $key => $value)
         {
-            $pdf->Cell(35,5,'Skill Name :',0,0,'L');  
+            $pdf->SetFont('helvetica','',12);
+            $pdf->Cell(35,5,'Skill Name ',0,0,'L');  
             $pdf->Cell(35,5,$value,0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Years Experience :',0,0,'L');    
+            $pdf->Cell(35,5,'Years Experience ',0,0,'L');    
             $pdf->Cell(35,5,$skillYearsExperience[$key],0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Level :',0,0,'L');   
+            $pdf->Cell(35,5,'Level ',0,0,'L');   
             $pdf->Cell(35,5,$skillLevel[$key],0,0,'L');
             $pdf->Ln();
             $pdf->Ln();            
         }    
 //Language
+$pdf->SetFont('Times', 'BI', 16);
 $pdf->Cell(0,5,'Language',0,0,'C');
 $pdf->Ln();
     foreach($langName as $key => $value)
         {
-            $pdf->Cell(35,5,'Name :',0,0,'L');    
+            $pdf->SetFont('helvetica','',12);
+            $pdf->Cell(35,5,'Name ',0,0,'L');    
             $pdf->Cell(35,5,$value,0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Spoken Level :',0,0,'L');    
+            $pdf->Cell(35,5,'Spoken Level ',0,0,'L');    
             $pdf->Cell(35,5,$langspeaking[$key],0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Reading Level :',0,0,'L');   
+            $pdf->Cell(35,5,'Reading Level ',0,0,'L');   
             $pdf->Cell(35,5,$langReading[$key],0,0,'L');
             $pdf->Ln();
-            $pdf->Cell(35,5,'Writing Level :',0,0,'L');   
+            $pdf->Cell(35,5,'Writing Level ',0,0,'L');   
             $pdf->Cell(35,5,$langwriting[$key],0,0,'L');
             $pdf->Ln();
         }
 
 //Reference
+$pdf->SetFont('Times', 'BI', 16);
 $pdf->Cell(0,5,'Reference',0,0,'C');
 $pdf->Ln();
-$pdf->Cell(35, 5, 'Name:');
+$pdf->SetFont('helvetica','',12);
+$pdf->Cell(35, 5, 'Name');
 $pdf->Cell(35, 5, $name);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Phone:');
+$pdf->Cell(35, 5, 'Phone');
 $pdf->Cell(35, 5, $Phone);
 $pdf->Ln(6);
-$pdf->Cell(35, 5, 'Email:');
+$pdf->Cell(35, 5, 'Email');
 $pdf->Cell(35, 5, $mbox);
 $pdf->Ln(6);
 
